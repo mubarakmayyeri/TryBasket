5912a11080016f7bc8e60d6191ec8dcbc1d16462
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2022-11-09 01:20:38 +0530

    basic shop page setup

--- shop/views.py
+++ shop/views.py
@@ -8,14 +8,32 @@ def home(request):
   products = Product.objects.all().filter(is_available=True)
   featured_categories = Sub_Category.objects.all().filter(is_featured=True)[:5]
   featured_products = Product.objects.all().filter(is_featured=True)[:8]
-  latest_products = Product.objects.all().filter(is_featured=True).order_by('-created_date')[:3]
+  latest_products_1 = Product.objects.all().order_by('-created_date')[:3]
+  latest_products_2 = Product.objects.all().order_by('-created_date')[3:6]
   
   context = {
     'categories' : categories,
     'products': products,
     'featured_categories': featured_categories,
     'featured_products': featured_products,
-    'latest_products' : latest_products,
+    'latest_products_1' : latest_products_1,
+    'latest_products_2' : latest_products_2,
   }
   
   return render(request, 'home.html', context)
+
+def shop(request):
+  categories = Category.objects.all()
+  products = Product.objects.all().filter(is_available=True).order_by('product_name')
+  latest_products_1 = Product.objects.all().order_by('-created_date')[:3]
+  latest_products_2 = Product.objects.all().order_by('-created_date')[3:6]
+  poduct_count = products.count()
+  
+  context = {
+    'categories' : categories,
+    'products':products,
+    'latest_products_1' : latest_products_1,
+    'latest_products_2' : latest_products_2,
+    'poduct_count':poduct_count
+  }
+  return render(request, 'shop.html', context)
