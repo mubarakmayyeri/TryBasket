b7dfa3b9374dd2f19abf5a5184d280eabdb6739b
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2022-11-10 01:49:22 +0530

    changed id args with slug in admin views

--- adminPanel/urls.py
+++ adminPanel/urls.py
@@ -12,12 +12,12 @@ urlpatterns = [
   path('categories', views.categories, name='categories'),
   path('addCategory', views.addCategory, name='addCategory'),
   path('<str:slug>/editCategory', views.editCategory, name='editCategory'),
-  path('<int:id>/deleteCategory', views.deleteCategory, name='deleteCategory'),
+  path('<str:slug>/deleteCategory', views.deleteCategory, name='deleteCategory'),
   
-  path('<int:id>/subCategories', views.subCategories, name='subCategories'),
-  path('<int:id>/addSubCategory', views.addSubCategory, name='addSubCategory'),
+  path('<str:category_slug>/subCategories', views.subCategories, name='subCategories'),
+  path('<str:category_slug>/addSubCategory', views.addSubCategory, name='addSubCategory'),
   path('<str:slug>/editSubCategory', views.editSubCategory, name='editSubCategory'),
-  path('<int:id>/deleteSubCategory', views.deleteSubCategory, name='deleteSubCategory'),
+  path('<str:slug>/deleteSubCategory', views.deleteSubCategory, name='deleteSubCategory'),
   
   path('products', views.products, name='products'),
   path('<int:id>/deleteProduct', views.deleteProduct, name='deleteProduct'),
